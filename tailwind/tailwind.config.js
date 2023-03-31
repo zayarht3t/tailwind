1e5e8b13a41868675b6aa007f689dd6b356fb7bf
zayarht3t <122688526+zayarht3t@users.noreply.github.com>
2023-03-31 12:27:52 +0630

    Add files via upload

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html',"./build/js/*.js"],
+  theme: {
+    extend: {
+      keyframes: {
+        'open-menu': {
+          '0%': {transform: 'scaleY(0)'},
+          '80%': {transform: 'scaleY(1.2)'},
+          '100%': {transform: 'scaleY(1)'}
+        }
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards'
+      }
+    },
+  },
+  plugins: [],
+}
+
